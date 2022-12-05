902948076fd9bd1a7f8192018c2eb916c989565b
CxCore.xyz <91425802+CxCore-xyz@users.noreply.github.com>
2022-12-05 19:35:31 +0200

    +Fixed Error & Updated The Core
    +Fixed Error & Updated The Core

--- server/main.lua
+++ server/main.lua
@@ -1,27 +1,25 @@
-GBCore = nil
-TriggerEvent('GBCore:GetObject', function(obj) GBCore = obj end)
-
+local QBCore = exports['qb-core']:GetCoreObject()
 local timeOut = false
 
-GBCore.Functions.CreateCallback('cpbea:server:Reward', function(source)
+QBCore.Functions.CreateCallback('cpbea:server:Reward', function(source)
     local src = source
-    local Player = GBCore.Functions.GetPlayer(src)
+    local Player = QBCore.Functions.GetPlayer(src)
     local otherchance = math.random(1, 4)
     local odd = math.random(1, 4)
     local itemType = math.random(#Config.RewardTypes)
+end)
 
-	
 if Config.RewardTypes[itemType].type == "item" then
 	local item = math.random(1, #Config.CuReward)
 	local amount = math.random(Config.CuReward[item]["amount"]["min"], Config.CuReward[item]["amount"]["max"])
 		if Player.Functions.AddItem(Config.CuReward[item]["item"], amount) then
-				TriggerClientEvent('gb-inventory:client:ItemBox', src, GBCore.Shared.Items[Config.CuReward[item]["item"]], 'add')
+		    TriggerClientEvent('inventory:client:ItemBox', src, GBCore.Shared.Items[Config.CuReward[item]["item"]], 'add')
 		else
-				TriggerClientEvent('GBCore:Notify', src, 'Inventory is full..', 'error')
+		   TriggerClientEvent('QBCore:Notify', src, 'Inventory is full..', 'error')
 		end
         elseif Config.RewardTypes[itemType].type == "money" then
 		local worth = math.random(10, 50)
-				Player.Functions.AddMoney("cash", worth, "weed-reward-money")
+		Player.Functions.AddMoney("cash", worth)
         end
 end)
 
